47f7d426e6521e976318d3344f99bfeb6ecda398
Martin Ciruzzi <p.ciruzzi@hotmail.com>
2015-10-25 16:04:43 -0300

    Initial Commit

---
+++ SolsticeExercise/app/src/androidTest/java/solstice/exercise/solsticeexercise/ApplicationTest.java
@@ -0,0 +1,13 @@
+package solstice.exercise.solsticeexercise;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ SolsticeExercise/app/src/main/java/solstice/exercise/solsticeexercise/ContactsActivityFragment.java
@@ -0,0 +1,19 @@
+package solstice.exercise.solsticeexercise;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ContactsActivityFragment extends Fragment {
+
+    public ContactsActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_contacts, container, false);
+    }
+}
